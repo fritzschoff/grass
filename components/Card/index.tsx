471a86549fa31774b884840b54eef272a29f655e
fritzschoff <max.j.f@gmx.de>
2021-11-06 12:17:58 +0100

    implemented styled components

---
+++ components/Card/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+interface CardProps {
+  inspect?: () => boolean;
+  position?: "hand" | "market";
+}
+
+export default function Card({ inspect }: CardProps) {
+  return <CardWrapper onClick={inspect}></CardWrapper>;
+}
+
+const CardWrapper = styled.div`
+  border: 1px solid black;
+  max-width: 200px;
+  width: 100%;
+  height: auto;
+`;
